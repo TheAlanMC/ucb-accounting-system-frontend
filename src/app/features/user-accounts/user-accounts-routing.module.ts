7eb585e7a15cfe5e10474f97116a9cbbd87d92c7
Gutber Blanco <gutber.blanco@ucb.edu.bo>
2023-09-14 17:36:12 -0400

    UaS-86 template user-info

--- src/app/features/user-accounts/user-accounts-routing.module.ts
+++ src/app/features/user-accounts/user-accounts-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user/profile', component: UserInfoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/user-accounts/user-accounts.module.ts
+++ src/app/features/user-accounts/user-accounts.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserAccountsRoutingModule } from './user-accounts-routing.module';
-
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule} from 'primeng/menubar';
+import { BarsIcon } from 'primeng/icons/bars';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { ImageModule } from 'primeng/image';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserInfoComponent
+  ],
   imports: [
     CommonModule,
-    UserAccountsRoutingModule
+    UserAccountsRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    MenubarModule,
+    BarsIcon,
+    AvatarModule,
+    AvatarGroupModule,
+    ImageModule
   ]
 })
 export class UserAccountsModule { }
