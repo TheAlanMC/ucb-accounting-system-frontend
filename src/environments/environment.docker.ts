68b46655e10bf6fc5eaa09dd19bf43b473a3b097
Chris Apaza <chrisalanapazaa@gmail.com>
2023-09-18 22:46:47 -0400

    added configuration to deploy with docker

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    KEYCLOAK_URL: 'http://68.183.126.58:8090',
+    KEYCLOAK_REALM: 'ucb-accounting-system',
+    KEYCLOAK_CLIENT_ID: 'uas-frontend',
+  
+    API_URL: 'http://68.183.126.58/backend',
+    ANGULAR_URL: 'http://68.183.126.58'
+  };
+  
