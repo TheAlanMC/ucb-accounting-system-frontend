e178bbbea2bc4ad6fd0d5ab26451681f178b8114
Gutber Blanco <gutber.blanco@ucb.edu.bo>
2023-10-09 16:40:09 -0400

    UAS-174 Design Created

--- src/app/features/journal-book/journal-book-routing.module.ts
+++ src/app/features/journal-book/journal-book-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListJournalentriesGeneratedComponent } from './components/list-journalentries-generated/list-journalentries-generated.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list/journal/g', component: ListJournalentriesGeneratedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/journal-book/journal-book.module.ts
+++ src/app/features/journal-book/journal-book.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { JournalBookRoutingModule } from './journal-book-routing.module';
+import { ListJournalentriesGeneratedComponent } from './components/list-journalentries-generated/list-journalentries-generated.component';
+import { TableModule } from 'primeng/table';
+import { TagModule} from 'primeng/tag';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListJournalentriesGeneratedComponent
+  ],
   imports: [
     CommonModule,
-    JournalBookRoutingModule
+    JournalBookRoutingModule,
+    TableModule,
+    TagModule
+
   ]
 })
 export class JournalBookModule { }
